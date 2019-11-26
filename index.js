99734f155069a71dcf45fa7d208196eb0ab0e921
Poalim.web <poalim.web@Poalimwebs-Mac-mini.local>
2019-11-26 07:09:22 -0800

    enable cors using npm module

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var keys = require('./keys');
 var app = express();
+var cors = require('cors');
 const port = process.env.PORT || 8080;
 
-app.use( (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});
+app.use(cors());
+
+// app.use( (req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//     next();
+// });
 
 app.get('/credit/', () => {
     next()
